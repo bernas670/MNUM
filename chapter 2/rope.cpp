db3c53bae78efdf391fc599686119b989ace86c9
Bernardo Santos <bernardosantos670@gmail.com>
2018-10-20 21:53:58 +0100

    added rope method

--- chapter 2/rope.cpp
+++ chapter 2/rope.cpp
@@ -7,31 +7,32 @@ double const ERROR = pow(10, -5);
 
 double f(double x) { return x - 2 * log(x) - 5; }
 
-void bissection(double lBound, double hBound) {
+void rope(double lBound, double hBound) {
 
-  double xn = 0;
+  double xn;
   unsigned int counter = 0;
 
-  while ( abs(hBound - lBound) > ERROR ) {
-
+  while ( abs(hBound - lBound) > ERROR )
+  {
     counter++;
 
-    xn = (lBound + hBound) / 2;
+    xn = (lBound * f(hBound) - hBound * f(lBound)) / (f(hBound) - f(lBound));
 
-    if ( f(lBound) * f(xn) < 0 )
+    if ( f(lBound) * f(xn) < 0)
       hBound = xn;
     else
       lBound = xn;
   }
 
-  cout << "Root value : " << xn << endl;
+  cout << "Root : " << xn << endl;
   cout << "Number of Iterations : " << counter << endl;
+
 }
 
 int main() {
 
-  bissection(0.01, 1);
-  bissection(9, 10);
+  rope(0.01, 1);
+  rope(9, 10);
 
   return 0;
 }
