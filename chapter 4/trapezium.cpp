0a5bbe30200abab29a6cf140705fa0bde5bd53df
Bernardo Santos <bernardosantos670@gmail.com>
2018-10-21 14:17:14 +0100

    added trapezium method for single integrals

---
+++ chapter 4/trapezium.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double PI = acos(-1);
+
+double f(double x) { return sin(x); }
+
+void trapezium(double lBound, double hBound, unsigned int intervals) {
+
+  double h = (hBound - lBound) / intervals;
+  double sum = f(lBound) + f(hBound);
+
+  for (unsigned int i = 1; i < intervals; i++) {
+    sum += 2 * f(lBound + h * i);
+  }
+
+  sum = (h / 2) * sum;
+
+  cout << "Area : " << sum << endl;
+}
+
+int main() {
+
+  trapezium(0, PI, 10);
+  trapezium(0, PI, 100);
+  trapezium(0, PI, 1000);
+
+  cout << endl;
+
+  trapezium(PI/2, PI, 10);
+  trapezium(PI/2, PI, 100);
+  trapezium(PI/2, PI, 1000);
+
+  return 0;
+}
